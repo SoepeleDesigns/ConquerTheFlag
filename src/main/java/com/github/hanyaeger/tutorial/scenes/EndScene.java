709187bb0a1aa415d293ff6a8b3576a9e6418e8a
CasparStins <stinscaspar@gmail.com>
2024-03-23 17:51:07 +0100

    EndScene finished

--- src/main/java/com/github/hanyaeger/tutorial/scenes/EndScene.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/EndScene.java
@@ -1,6 +1,8 @@
 package com.github.hanyaeger.tutorial.scenes;
 
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.StaticScene;
+import com.github.hanyaeger.tutorial.Button;
 import com.github.hanyaeger.tutorial.ConquerTheFlag;
 
 public class EndScene extends StaticScene {
@@ -13,12 +15,14 @@ public class EndScene extends StaticScene {
 
     @Override
     public void setupScene() {
-        setBackgroundAudio("audio/ocean.mp3");
+        //setBackgroundAudio("audio/ocean.mp3");
         setBackgroundImage("backgrounds/background3.jpg");
     }
 
     @Override
     public void setupEntities() {
-
+        Button button = new Button(
+                new Coordinate2D(getWidth() / 3,getHeight() / 2), conquerTheFlag);
+        addEntity(button);
     }
 }
